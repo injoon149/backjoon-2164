c70100de68f47813a2e8b111ef8b8657d2c52b0b
injoon149 <dlswns8679@naver.com>
2022-07-25 18:11:22 +0900

    초기 커밋.

---
+++ ConsoleApplication19.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+	queue<int> q;
+	int n = 0;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		q.push(i);
+	}
+	while (1)
+	{
+		if (q.size() == 1) break;
+		else
+		{
+			q.pop();
+			int k = q.front();
+			q.pop();
+			q.push(k);
+
+			
+
+		}
+	}
+
+	cout << q.front() << endl;
+
+	return 0;
+}
